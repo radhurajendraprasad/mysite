b651080f9d3bf5aafba88825f5007472ec88a96a
root <root@10CPU0157L.amritavidya.edu>
2016-06-01 16:26:41 +0530

    website

--- mysite/settings.py
+++ mysite/settings.py
@@ -119,3 +119,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = '/media/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (MEDIA_ROOT,)
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
