97535f7e8b8e7b788d3220712daac9991e18aa42
radhurajendraprasad <radhikapr17@gmail.com>
2016-06-02 12:00:08 +0530

    dropdown

--- mysite/settings.py
+++ mysite/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'register'
 ]
-
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
